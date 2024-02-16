105f5771736bb6bc8b0f30794f7096a0bf0edce3
Collin Coil <collincoil11@gmail.com>
2024-02-16 09:10:37 -0500

    Code cleanup

--- Code/query_s2orc.py
+++ Code/query_s2orc.py
@@ -7,25 +7,28 @@ The abstracts in this json file will be integral to extend the pretraining of th
 import requests
 import json
 
-# This query is designed to get as many articles as possible that relate to the Affordable Care Act or federal health insurance during President Obama or President Trump's term
-query = 'Obamacare | (Affordable Care Act) | PPACA | Medicare | Medicaid | (CHIP + Obama) | (ACA + Obama) | (marketplace + health + insurance) | (Skinny Repeal) | (Trump + Health + Insurance) | (Obama + Health + Insurance)'
-fields = 'title,publicationTypes,publicationDate,abstract'
 
-url = f"http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}&year=2007-"
-r = requests.get(url).json()
+def main():
+    query = 'Obamacare | (Affordable Care Act) | PPACA | Medicare | Medicaid | (CHIP + Obama) | (ACA + Obama) | (marketplace + health + insurance) | (Skinny Repeal) | (Trump + Health + Insurance) | (Obama + Health + Insurance)'
+    fields = 'title,publicationTypes,publicationDate,abstract'
+    
+    url = f"http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={fields}&year=2007-"
+    r = requests.get(url).json()
+    
+    print(f"Will retrieve an estimated {r['total']} documents")
+    retrieved = 0
+    
+    with open(f"Data/papers.jsonl", "a") as file:
+        while True:
+            if "data" in r:
+                retrieved += len(r["data"])
+                print(f"Retrieved {retrieved} papers...")
+                for paper in r["data"]:
+                    print(json.dumps(paper), file=file)
+            if "token" not in r:
+                break
+            r = requests.get(f"{url}&token={r['token']}").json()
+    
+    print(f"Done! Retrieved {retrieved} papers total")
 
-print(f"Will retrieve an estimated {r['total']} documents")
-retrieved = 0
-
-with open(f"Data/papers.jsonl", "a") as file:
-    while True:
-        if "data" in r:
-            retrieved += len(r["data"])
-            print(f"Retrieved {retrieved} papers...")
-            for paper in r["data"]:
-                print(json.dumps(paper), file=file)
-        if "token" not in r:
-            break
-        r = requests.get(f"{url}&token={r['token']}").json()
-
-print(f"Done! Retrieved {retrieved} papers total")
+main()
