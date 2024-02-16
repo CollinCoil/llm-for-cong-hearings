f145d3515c134b447ddb4699ee71a9c518e78a00
Collin Coil <collincoil11@gmail.com>
2024-02-16 16:06:48 -0500

    tsdae pretraining done

--- Code/model_training.py
+++ Code/model_training.py
@@ -6,14 +6,49 @@ and the pretraining corpora generated from using the generate_pretrainig_corpora
 '''
 # TODO: Add methods for model extended pretraining and fine tuning
 
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer, LoggingHandler
+from sentence_transformers import models, util, datasets, evaluation, losses
+from torch.utils.data import DataLoader
+import numpy as np
+import nltk
+nltk.download('punkt')
+
+# The following code continues pretraining of a pretrained model with TSDAE, 
+# See https://www.sbert.net/examples/unsupervised_learning/TSDAE/README.html
 
 def pretrain_model(model, data):
-    model = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
-    pass
+    model_name = "roberta"
+
+    # Create the special denoising dataset that adds noise on-the-fly
+    train_dataset = datasets.DenoisingAutoEncoderDataset(data)
+
+    # DataLoader to batch your data
+    train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+
+    # Use the denoising auto-encoder loss
+    train_loss = losses.DenoisingAutoEncoderLoss(
+        model, decoder_name_or_path=model_name, tie_encoder_decoder=True
+        )
+    
+    # Call the fit method
+    model.fit(
+        train_objectives=[(train_dataloader, train_loss)],
+        epochs=10,
+        weight_decay=0,
+        scheduler="constantlr",
+        optimizer_params={"lr": 3e-5},
+        show_progress_bar=True,
+    )
+
+    model.save("models/pretrained_model")
 
 
 
 def fine_tune_model(model, data):
     pass
 
+
+
+pretraining_data = np.load("Data/abstracts.npy")
+model = SentenceTransformer("all-distilroberta-v1")
+pretrain_model(model, pretraining_data)
