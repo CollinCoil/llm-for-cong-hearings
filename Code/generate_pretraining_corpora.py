9d5e63a54755c3acbf3471037810bec63f2d74ac
Collin Coil <collincoil11@gmail.com>
2024-02-16 13:15:11 -0500

    Update code

--- Code/generate_pretraining_corpora.py
+++ Code/generate_pretraining_corpora.py
@@ -1,5 +1,50 @@
+'''
+This code accepts two inputs:
+    (1) the json file created by the query_s2orc.py file, and 
+    (2) the json file containing text extracted from the LHIC that will not be searched for comparison with witness statements. 
+
+The goal is to generate two corpora and output csv files that will be used in model_training.py for extended pre-training and fine tuning of the sentence transformers model. 
+'''
+import json
+import numpy as np
+import re
+
 # TODO: Generate two corpora:
-    # (1) S2ORC Abstracts on Obamacare
     # (2) Whatever texts are discarded in the LHIC
         # Use a zero-shot classification model to identify paragraphs in the discarded texts that are related to given topics, and use them to create paragraph pairings
             # Paragraphs with similar topics will be used to make pairs of similar paragraphs, paragraphs with different topics will be used to make pairs of different topics
+
+
+def generate_pretraining_corpus(data_path:str, output_path:str):
+    abstracts = list()
+    with open(data_path, "r") as jsonfile:
+        for line in jsonfile:
+            data = json.loads(line)
+            abstract = data.get("abstract")
+            
+            if abstract is not None:
+                line_skip = "\n"
+                abstract = re.sub(line_skip, " ", abstract)
+                abstract = abstract.strip()
+                abstracts.append(abstract)
+
+    abstract_array = np.array(abstracts)
+    np.save("Data/abstracts.npy", abstract_array)
+
+
+
+
+
+
+
+
+def generate_finetuning_corpus(data_path:str, output_path:str):
+    pass
+
+
+
+
+
+
+
+generate_pretraining_corpus("Data/papers.jsonl", "Data/abstracts.csv")
--- Code/model_training.py
+++ Code/model_training.py
@@ -1,1 +1,19 @@
+'''
+This file contains code used for extended pre-training and fine tuning of a sentence transformers model. 
+It needs a pretrained model (which can be found on https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models)
+and the pretraining corpora generated from using the generate_pretrainig_corpora.py file. 
+
+'''
 # TODO: Add methods for model extended pretraining and fine tuning
+
+from sentence_transformers import SentenceTransformer, util
+
+def pretrain_model(model, data):
+    model = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
+    pass
+
+
+
+def fine_tune_model(model, data):
+    pass
+
