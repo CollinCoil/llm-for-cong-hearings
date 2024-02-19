7cf68e4f32e5d4406216c555218e62083d23e289
Collin Coil <collincoil11@gmail.com>
2024-02-19 09:15:38 -0500

    initial tsne code

--- Code/t-sne-results.py
+++ Code/t-sne-results.py
@@ -1,2 +1,8 @@
 # TODO: Perform t-SNE on paragraphs related to Obamacare on the W&LHIC to see how the embeddings are distributed in space
 # TODO: Use these t-SNE embeddings to see how witness statements compare to LHIC embeddings
+
+from sklearn.manifold import TSNE
+
+def create_tsne_embeddings(dataframe, perplexity = 50):
+    tsne_embeddings = TSNE(n_componsnts = 2, perplexity = perplexity).fit_transform(dataframe)
+    return tsne_embeddings
