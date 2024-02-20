638350185c1facc3cec11962169b61d18fd3865e
Collin Coil <collincoil11@gmail.com>
2024-02-20 12:43:20 -0500

    Nearly finished topic modeling

--- Code/setup.py
+++ Code/setup.py
@@ -1,3 +1,3 @@
 # sentence transformers, numpy, re, json
-# nltk, keybert
+# nltk, keybert, gensim
 # Microsoft Visual C++ Redistributable, not a package to download
--- Code/topic_modeling.py
+++ Code/topic_modeling.py
@@ -1,25 +1,86 @@
-# TODO: Use several techniques to generate topics for each file in the W&LHIC
-# TODO: Look into KeyBERT and LDA
-# Output: 
-    # (1) List of keywords for every document in W&LHIC
-        # This will allow us to make silly little word clouds for topics most often discussed by witnesses against the three branches
-
 from keybert import KeyBERT
-from sklearn.decomposition import LatentDirichletAllocation
+import numpy as np
+import string
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')  
+nltk.download('omw-1.4')  
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim import corpora
+from gensim.models import LdaModel
 
-def extract_keywords(document_list, n_keywords):
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation)
+lemma = WordNetLemmatizer()
+
+# Code to extract keywords using KeyBert
+def keybert_keywords(document_list, n_keywords):
     kw_model = KeyBERT()
-    keywords = kw_model.extract_keywords(document_list, keyphrase_ngram_range = (1,2), stop_words = "english",
-                                         top_n = n_keywords, diversity = 0.5)
+    keybert_results = kw_model.extract_keywords(document_list, keyphrase_ngram_range = (1,2), stop_words = "english",
+                                         top_n = n_keywords, diversity = 0.5, use_mmr=True)
+    keywords = list()
+    for document in keybert_results:
+        document_keywords = list()
+        for word, value in document:
+            document_keywords.append(word)
+
+        keywords.append(document_keywords)
+    return keywords
+
+
+# Code to perform LDA is adapted from https://www.datacamp.com/tutorial/what-is-topic-modeling with some changes. 
+
+# Remove stopwords, punctuation, and normalize the corpus
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = "".join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+def create_lda_inputs(document_list):
+    clean_documents = [clean(doc).split() for doc in document_list]
+    dictionary = corpora.dictionary.Dictionary(clean_documents)
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in clean_documents]
+    return dictionary, doc_term_matrix
+
+def lda_keywords(document_list, n_keywords):
+    dictionary, document_term_matrix = create_lda_inputs(document_list)
+    lda_model = LdaModel(document_term_matrix, id2word = dictionary, num_topics = n_keywords, random_state = 2024)
+    keywords = list()
+    for doc in document_term_matrix:
+        topics = lda_model[doc] # Gets topics for each document
+        document_keywords = list()
+        for topic, prob in topics: 
+            topic_list = lda_model.show_topic(topic, topn=n_keywords)
+            for word, value in topic_list:
+                document_keywords.append(word)
+
+        keywords.append(document_keywords)
+            
+    return keywords
+
+
+# Function to easily generate keywords using both Keybert and LDA
+def generate_keywords(document_list, n_keybert=1, n_lda=1):
+    if n_keybert == 0 and n_lda == 0:
+        raise ValueError("At least one of n_keybert and n_lda needs to be greater than 0.")
+    if n_keybert > 0:
+        keybert_results = np.array(keybert_keywords(document_list, n_keybert))
+    if n_lda > 0:
+        lda_results = np.array(lda_keywords(document_list, n_lda))
+    if n_keybert > 0 and n_lda > 0:
+        keywords = np.hstack((keybert_results, lda_results))
+        return keywords
+    elif n_keybert == 0:
+        return lda_results
+    else:
+        return keybert_results
     
-    return keywords
+# Function to save keyword dataframe
+def save_keywords(keywords, file_path_name):
+    np.savetxt(file_path_name, keywords, delimiter=",")
 
 
-def lda(document_list, n_keywords):
-    model = LatentDirichletAllocation(n_components = n_keywords, learning_method="online", random_state=2024)
-    keywords = model.fit_transform(document_list)
-    return keywords
-
-documents = ["How often to evaluate perplexity. Only used in fit method. set it to 0 or negative number to not evaluate perplexity in training at all. Evaluating perplexity can help you check convergence in training process, but it will also increase total training time. Evaluating perplexity in every iteration might increase training time up to two-fold.", "In natural language processing, latent Dirichlet allocation (LDA) is a Bayesian network (and, therefore, a generative statistical model) for modeling automatically extracted topics in textual corpora. The LDA is an example of a Bayesian topic model. In this, observations (e.g., words) are collected into documents, and each word's presence is attributable to one of the document's topics. Each document will contain a small number of topics."]
-extract_keywords(documents, 10)
-lda(documents, 10)
+keywords = generate_keywords("Data/corpus", 5, 5) # TODO: Add this numpy array with the corpus here
+save_keywords(keywords, "Data/corpus_keywords.csv")
