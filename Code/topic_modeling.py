1580cf8ac2d2c3db3d0aba5f138a616156463292
Collin Coil <collincoil11@gmail.com>
2024-03-04 12:01:28 -0500

    Add ability to read jsonl files

--- Code/topic_modeling.py
+++ Code/topic_modeling.py
@@ -8,6 +8,7 @@ the corpus.
 
 
 from keybert import KeyBERT
+import json
 import numpy as np
 import string
 import nltk
@@ -18,7 +19,6 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from gensim import corpora
 from gensim.models import LdaModel
-from sentence_transformers import SentenceTransformer
 
 stop = set(stopwords.words('english'))
 exclude = set(string.punctuation)
@@ -27,7 +27,7 @@ lemma = WordNetLemmatizer()
 # Code to extract keywords using KeyBert
 def keybert_keywords(document_list, n_keywords):
     kw_model = KeyBERT()
-    keybert_results = kw_model.extract_keywords(document_list, keyphrase_ngram_range = (1,1), stop_words = "english",
+    keybert_results = kw_model.extract_keywords(document_list, keyphrase_ngram_range = (1,2), stop_words = "english",
                                          top_n = n_keywords, diversity = 0.5, use_mmr=True)
     keywords = list()
     for document in keybert_results:
@@ -36,7 +36,7 @@ def keybert_keywords(document_list, n_keywords):
             document_keywords.append(word)
 
         keywords.append(document_keywords)
-    return keywords
+    return np.array(keywords)
 
 
 # Code to perform LDA is adapted from https://www.datacamp.com/tutorial/what-is-topic-modeling with some changes. 
@@ -68,7 +68,7 @@ def lda_keywords(document_list: list, n_keywords: int):
 
         keywords.append(document_keywords)
             
-    return keywords
+    return np.array(keywords)
 
 
 # Wrapper function to easily generate keywords using both Keybert and LDA
@@ -89,8 +89,37 @@ def generate_keywords(document_list:list, n_keybert:list = 1, n_lda:list = 1):
     
 # Function to save keyword dataframe
 def save_keywords(keywords, file_path_name):
-    np.savetxt(file_path_name, keywords, delimiter=",")
+    np.savetxt(file_path_name, keywords, delimiter=",", fmt="%s")
 
+# Function to read in json lines file if that is data format
+def read_jsonl(file_path_name):
+    texts = list()
+    ids = list()
+    with open(file_path_name, "r") as jsonfile:
+        for line in jsonfile:
+            data = json.loads(line)
+            text = data.get("speech") # May need to change this string to whatever the text is saved as in the jsonl file
+            texts.append(text)
+            id = data.get("speech_id")
+            ids.append(str(id))
+            
 
-keywords = generate_keywords("Data/file_path.npy", 5, 5) # TODO: Add this numpy array or json file with the corpus here
-save_keywords(keywords, "Data/corpus_keywords.csv")
+    text_array = np.array(texts)
+    return text_array, ids
+
+# Function to add speech id to keywords for easier tracking
+def add_ids_to_keywords(ids, keywords):
+    keywords_with_id = np.empty((keywords.shape[0], keywords.shape[1] + 1), dtype=object)
+    keywords_with_id[:, 1:] = keywords
+    for index, id in enumerate(ids):
+        keywords_with_id[index, 0] = id
+    return keywords_with_id
+
+congresses = ["110", "111", "112", "113", "114"]
+
+for term in congresses: 
+    text, ids = read_jsonl("Data/relevant_speeches_%s.jsonl" % term) # Replace this read_jsonl function call with something to open other data type if necessary
+    keywords = generate_keywords(text, 3, 3)
+    keywords_with_id = add_ids_to_keywords(ids, keywords)
+    output_file_name = "Data/congressional_speech_%s_keywords.csv" % term
+    save_keywords(keywords_with_id, output_file_name)
