44373b702544680a163779e4290771bc961badd1
Collin Coil <collincoil11@gmail.com>
2024-03-04 15:18:57 -0500

    Remove LDA

--- Code/topic_modeling.py
+++ Code/topic_modeling.py
@@ -1,7 +1,7 @@
 '''
-This file contains code used for extended extracting the topics of the corpus. It does this through two different techniques: (1) KeyBERT and (2) Latent Dirichlet Allocation (LDA).
-Keybert is a more modern, sophisticated tool, and LDA is a classic tool for topic modeling. This python file takes the json file containing the corpus and returns
-the keywords for each file. You can choose to use both KeyBERT and LDA or either one. The output is a csv file with the keywords that align with the indices of each document in 
+This file contains code used for extended extracting the topics of the corpus. It does this using KeyBERT.
+Keybert is a more modern, sophisticated tool for keyword extraction. This python file takes the json file containing the corpus and returns
+the keywords for each file. The output is a csv file with the keywords that align with the indices of each document in 
 the corpus. 
 
 '''
@@ -10,19 +10,7 @@ the corpus.
 from keybert import KeyBERT
 import json
 import numpy as np
-import string
-import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')  
-nltk.download('omw-1.4')  
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from gensim import corpora
-from gensim.models import LdaModel
 
-stop = set(stopwords.words('english'))
-exclude = set(string.punctuation)
-lemma = WordNetLemmatizer()
 
 # Code to extract keywords using KeyBert
 def keybert_keywords(document_list, n_keywords):
@@ -38,54 +26,6 @@ def keybert_keywords(document_list, n_keywords):
         keywords.append(document_keywords)
     return np.array(keywords)
 
-
-# Code to perform LDA is adapted from https://www.datacamp.com/tutorial/what-is-topic-modeling with some changes. 
-
-# Remove stopwords, punctuation, and normalize the corpus
-def clean(doc):
-    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-    punc_free = "".join(ch for ch in stop_free if ch not in exclude)
-    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-    return normalized
-
-def create_lda_inputs(document_list: list):
-    clean_documents = [clean(doc).split() for doc in document_list]
-    dictionary = corpora.dictionary.Dictionary(clean_documents)
-    doc_term_matrix = [dictionary.doc2bow(doc) for doc in clean_documents]
-    return dictionary, doc_term_matrix
-
-def lda_keywords(document_list: list, n_keywords: int):
-    dictionary, document_term_matrix = create_lda_inputs(document_list)
-    lda_model = LdaModel(document_term_matrix, id2word = dictionary, num_topics = n_keywords, random_state = 2024)
-    keywords = list()
-    for doc in document_term_matrix:
-        topics = lda_model[doc] # Gets topics for each document
-        document_keywords = list()
-        for topic, prob in topics: 
-            topic_list = lda_model.show_topic(topic, topn=n_keywords)
-            for word, value in topic_list:
-                document_keywords.append(word)
-
-        keywords.append(document_keywords)
-            
-    return np.array(keywords)
-
-
-# Wrapper function to easily generate keywords using both Keybert and LDA
-def generate_keywords(document_list:list, n_keybert:list = 1, n_lda:list = 1):
-    if n_keybert == 0 and n_lda == 0:
-        raise ValueError("At least one of n_keybert and n_lda needs to be greater than 0.")
-    if n_keybert > 0:
-        keybert_results = np.array(keybert_keywords(document_list, n_keybert))
-    if n_lda > 0:
-        lda_results = np.array(lda_keywords(document_list, n_lda))
-    if n_keybert > 0 and n_lda > 0:
-        keywords = np.hstack((keybert_results, lda_results))
-        return keywords
-    elif n_keybert == 0:
-        return lda_results
-    else:
-        return keybert_results
     
 # Function to save keyword dataframe
 def save_keywords(keywords, file_path_name):
@@ -119,7 +59,7 @@ congresses = ["110", "111", "112", "113", "114"]
 
 for term in congresses: 
     text, ids = read_jsonl("Data/relevant_speeches_%s.jsonl" % term) # Replace this read_jsonl function call with something to open other data type if necessary
-    keywords = generate_keywords(text, 3, 3)
+    keywords = keybert_keywords(text, 3, 3)
     keywords_with_id = add_ids_to_keywords(ids, keywords)
     output_file_name = "Data/congressional_speech_%s_keywords.csv" % term
     save_keywords(keywords_with_id, output_file_name)
