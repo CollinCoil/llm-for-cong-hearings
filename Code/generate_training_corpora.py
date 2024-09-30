ba9f720506400582b81a3430798c08bfda9c179e
Collin Coil <collincoil11@gmail.com>
2024-09-30 11:25:36 -0400

    minor tweak

--- Code/generate_training_corpora.py
+++ Code/generate_training_corpora.py
@@ -47,7 +47,7 @@ def generate_pretraining_corpus(data_path:str):
 
 
 
-def generate_finetuning_corpus(directory_path:str, output_path:str = "output.jsonl", corpus_size:int = 150000):
+def generate_finetuning_corpus(directory_path:str, output_path:str = "output.jsonl", corpus_size:int = 200000):
   """
   This code creates a fine-tuning corpus based on the text files in a directory. These text files were prepared in the same manner as those of the WLHIC, but 
   they are not being used to search for witness testimony impact.
